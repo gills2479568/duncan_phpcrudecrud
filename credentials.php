0f88cea85c459b13fecf13fefc9a8e2ea08a898d
Duncan <gille668@umn.edu>
2026-03-13 15:47:52 +0000

    initial commit

---
+++ credentials.php
@@ -0,0 +1,8 @@
+<?php
+//Rember to populate data here
+//
+$servername = 'localhost';     //if apache on diff server from db or using Docker, set IP address
+$dbname = '';              //which db you're going to use
+$username = '';
+$password = '';
+?>
